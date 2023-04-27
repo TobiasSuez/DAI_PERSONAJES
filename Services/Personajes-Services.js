600a6127a2414cf3b8bfa124f67f6e52394b7c03
TOBIAS ARIEL SUEZ <46919362@ort>
2023-04-27 10:35:27 -0300

    algo hicimo

--- Services/Personajes-Services.js
+++ Services/Personajes-Services.js
@@ -15,26 +15,41 @@ getAll = async () => {
     }
     return returnArray;
 }
-
-/*insert = async (pizza) => {
+/*
+getById = async (id) => {
+    let returnEntity = null;
+    console.log('Estoy en: PersonajeService.getById(id)');
+    try {
+        let pool   = await sql.connect(config);
+        let result = await pool.request()
+                            .input('pId', sql.Int, id)
+                            .query('SELECT * FROM Personajes WHERE id = @pId');
+        returnEntity = result.recordsets[0][0];
+    } catch (error) {
+        console.log(error);
+    }
+    return returnEntity;
+}*/
+/*
+insert = async (Personaje) => {
     let rowsAffected = 0;
-    console.log('Estoy en: PizzaService.insert(pizza)');
+    console.log('Estoy en: Personajes-Service.insert(Personaje)');
 
     try {
         let pool = await sql.connect(config);
         let result = await pool.request()
-            .input('pNombre'     , sql.NChar , pizza?.nombre ?? '')
-            .input('pLibreGluten', sql.Bit   , pizza?.libreGluten ?? false)
-            .input('pImporte'    , sql.Float , pizza?.importe ?? 0)
-            .input('pDescripcion', sql.NChar , pizza?.descripcion ?? '')
-            .query(`INSERT INTO Pizzas (Nombre, LibreGluten, Importe, Descripcion) VALUES (@pNombre, @pLibreGluten, @pImporte, @pDescripcion)`);
+            .input('pNombre'     , sql.NChar , Personaje?.nombre ?? '')
+            .input('pLibreGluten', sql.Bit   , Personaje?.libreGluten ?? false)
+            .input('pImporte'    , sql.Float , Personaje?.importe ?? 0)
+            .input('pDescripcion', sql.NChar , Personaje?.descripcion ?? '')
+            .query(`INSERT INTO Personajes (Nombre, LibreGluten, Importe, Descripcion) VALUES (@pNombre, @pLibreGluten, @pImporte, @pDescripcion)`);
         rowsAffected = result.rowsAffected;
     } catch (error) {
         console.log(error);
     }
     return rowsAffected;
-}
-
+}*/
+/*
 update = async (pizza) => {
     let rowsAffected = 0;
     console.log('Estoy en: PizzaService.update(pizza)');
--- index.js
+++ index.js
@@ -1,42 +1,72 @@
-import config from '../dbconfig-env.js';
-import sql from 'mssql';
+import PersonajesServices  from './Services/Personajes-services.js'
+import Pelicula from './modulos/Pelicula.js'
+import Personajes from './modulos/Personajes.js'
 
-console.clear();
-console.log('\nconfig: ');
-console.log(config);
 
-test_01();
+await test_getAll();
+//await test_getById();
+//await test_insert();
+//await test_update();
+//await test_deleteById();
 
-async function test_01(){
-    let pool   = await sql.connect(config);
-    let result = await pool.request().query("SELECT TOP 2 * FROM Pizzas");
-    
-    // first recordset from result.recordsets
-    console.log('\nresult.recordset: ');
-    console.log(result.recordset)            
-    console.log('\nresult.recordsets[0]: ');
-    console.log(result.recordsets[0]);   
-    
-    // count of recordsets returned by the procedure
-    console.log('\nresult.recordsets.length: ');
-    console.log(result.recordsets.length);
+async function test_getAll(){
+    let svc = new PersonajeService();
+    let data;
 
-     // count of rows contained in first recordset
-    console.log('\nresult.recordsets[0].length: ');
-    console.log(result.recordsets[0].length); 
-    
-    // array of numbers, each number represents
-    console.log('\nresult.rowsAffected: ');
-    console.log(result.rowsAffected) ;         
-    
-    //  the number of rows affected by executed statements
-    // procedure return value
-    console.log('\nresult.returnValue: ');
-    console.log(result.returnValue);
+    data = await svc.getAll();
+    console.log(data);  //muestro todas las pizzas
 
-    // key/value collection of output values
-    console.log('\nresult.output: ');
-    console.log(result.output)                      
-    
-    process.exit();
+    //console.log(data[0].Id);
+    //console.log(data[0].id);
+    //console.log(data[0].Id);
 }
+/*
+async function test_getById(){
+    let svc = new PizzasService();
+    let data;
+    data = await svc.getById(2);
+    console.log(data);
+}
+
+async function test_insert(){
+    let svc = new PizzasService();
+    let data;
+    let nuevaPizza = new Pizza();
+    
+    nuevaPizza.nombre       = 'Pizza Guesa';
+    nuevaPizza.libreGluten  = true;
+    nuevaPizza.importe      = 750.50;
+    nuevaPizza.descripcion  = 'Pizza con sabor a hamburguesa';
+    console.log('\nnuevaPizza: ');
+    console.log(nuevaPizza);
+
+    data = await svc.insert(nuevaPizza);
+    console.log(data);
+}
+
+async function test_update(){
+    let svc = new PizzasService();
+    let data;
+    let laPizza;
+
+    laPizza = await svc.getById(12);
+    if (laPizza!= null){
+        laPizza.importe      = 10750.50;
+
+        data = await svc.update(laPizza);
+        console.log(data);
+    } else {
+        console.log('\laPizza: ');
+        console.log(laPizza);
+    }
+}
+
+async function test_deleteById(){
+    let svc = new PizzasService();
+    let data;
+
+    data = await svc.deleteById(12);
+    console.log(data);
+}*/
+
+process.exit();
